dfd9a915fc0342f3cf2d51b1e1f9b7226615e920
raphkm <rkariuki415@gmail.com>
2020-03-01 22:32:38 +0300

    added changes

---
+++ Readme.md
@@ -0,0 +1,51 @@
+### Tiki Taka Pizza
+
+# Project name:Tiki Taka PIZZA.
+
+# Project description:
+> You own a pizza place and as a way of trying to widen your customer base, you decide to create a web application where your customers order different sizes of pizzas with one or more toppings. 
+
+# App Screenshot.
+<img src="images/Screenshot.png">
+
+# Author(s) information: 
+> Raphael Kariuki
+
+# Setup instructions:
+>* Choose the size of pizza
+>* Choose what kind of crust.
+>* Choose the topping.
+>* Choose as many pizzas as you want.
+>* Delivery & Checkout.
+
+# Technologies used: 
+* html, 
+* css, 
+* bootstrap, 
+* js,
+* jQuery.
+
+# BDD:
+> Determine the prices for each pizza size.
+  Each pizza topping should have a set value depending on the size of the pizza.
+  Determine the cost for the crust
+  The total cost of a pizza should constitute, the crust, topping(s) and size of the pizza.
+  On checkout, the application displays the orders’ summary together with the total amount charged. For example, if the customer has made more than one pizza order, on checkout the application displays a summary of all the orders he/she has made and the total amount to pay for all his/her orders.
+ 
+# Known Bugs.
+> Multiple selection for toppings is not available.
+
+# Contact information.
+> To collaborate, reach me onrrkariuki415@gmail.com
+
+# License and Copyright information.
+> License
+MIT License
+
+Copyright (c) 2020 Raphael Kariuki.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ js/script.js
@@ -0,0 +1,147 @@
+var sizePrice, crustPrice, toppingPrice ;
+
+function sizeSelect(sizePricing,sizeSlecting){
+    this.sizePricing=sizePricing;
+    this.sizeSlecting=sizeSlecting;
+}
+
+function crustSelect(crustPricing,crustSelecting){
+    this.crustPricing=crustPricing;
+    this.crustSelecting=crustSelecting
+}
+
+function toppingSelect(toppingPricing,toppingSelecting){
+    this.toppingPricing=toppingPricing;
+    this.toppingSelecting=toppingSelect;
+}
+
+function pizzaNumberSelect(pieces){
+    this.pieces=pieces;
+}
+
+function finalPriceSubmition(finalPrice){
+    this.finalPrice=finalPrice;
+    return finalPrice;
+}
+
+function finalPriceDelivery(deliveryFinalPrice){
+    this.deliveryFinalPrice=deliveryFinalPrice;
+}
+$(document).ready(function(){
+
+
+   $(".check_size").click(function(){
+       var selectedSize = $("input[name='size']:checked").val();
+       if(selectedSize=='small'){
+        sizePrice = 250;
+        }
+        else if(selectedSize=='medium'){
+            sizePrice = 500;
+         }
+         else if(selectedSize=='large'){
+            sizePrice = 1000;
+         }
+         sizePricing = sizePrice;
+         sizeSlecting = selectedSize;
+         $("#price_show1").text(sizePricing);
+         console.log(sizePricing);
+         console.log(sizeSlecting)
+   });
+
+
+    $(".check_crust").click(function(){
+        var selectedCrust = $("input[name='crust']:checked").val();
+        if(selectedCrust=='thin'){
+            crustPrice = 12;
+        }
+        else if(selectedCrust=='thick'){
+            crustPrice = 30;
+        }
+        else if(selectedCrust=='stuffed'){
+            crustPrice = 35;
+        }
+        else if(selectedCrust=='crispy'){
+            crustPrice = 20;
+        }
+        else if(selectedCrust=='cheese'){
+            crustPrice = 25;
+        }
+        else if(selectedCrust=='gluten'){
+            crustPrice = 35;
+        }
+        crustPricing = crustPrice;
+        crustSelecting = selectedCrust;
+        $("#price_show1").text(sizePricing+crustPricing);
+        console.log(crustPricing);
+        console.log(crustSelecting);
+    });
+
+
+    $(".check_topping").click(function(){
+        var selectedTopping = $("input[name='topping']:checked").val();
+        if(selectedTopping=='pepperoni'){
+            toppingPrice = 100;
+        }
+        else if(selectedTopping=='mushrooms'){
+            toppingPrice = 75;
+        }
+        else if(selectedTopping=='onions'){
+            toppingPrice = 50;
+        }
+        else if(selectedTopping=='sausage'){
+            toppingPrice = 95;
+        }
+        else if(selectedTopping=='bacon'){
+            toppingPrice = 110;
+        }
+        else if(selectedTopping=='extra_cheese'){
+            toppingPrice = 50;
+        }
+        else if(selectedTopping=='black_olives'){
+            toppingPrice = 80;
+        }
+        else if(selectedTopping=='pineapple'){
+            toppingPrice = 75;
+        }
+        else if(selectedTopping=='green_peppers'){
+            toppingPrice = 65;
+        }
+        else if(selectedTopping=='spinach'){
+            toppingPrice = 60;
+        }
+        toppingPricing = toppingPrice;
+        toppingSelecting = selectedTopping;
+        $("#price_show1").text(sizePricing+crustPricing+toppingPricing);
+        console.log(toppingPricing);
+        console.log(toppingSelecting);
+    });
+
+
+    $("#pizza_number").change(function(){
+        var selectedPizzaNumber = $("#pizza_number").val();
+        pieces = selectedPizzaNumber;
+        $("#price_show1").text((sizePricing+crustPricing+toppingPricing)*pieces);
+        console.log(pieces);
+    });
+
+    $(".open").click(function(){
+        var allPrice = ((sizePricing+crustPricing+toppingPricing)*pieces);
+        finalPrice = allPrice;
+        //finalPrice = new finalPriceSubmition(finalPrice);
+        console.log(finalPrice);
+
+    });
+
+   $("#delivery_no").click(function(){
+    alert("This is your receipt, take a screenshot of it and display it at the counter of Tiki Taka Pizza.\n Your pizza order was.\n"+sizeSlecting+"@: "+sizePricing+"\n"+crustSelecting+"@: "+crustPricing+"\n"+toppingSelecting+"@: "+toppingPricing+"\n"+"Number of pizza's: "+pieces+"\n"+"Your total cost is. KSH: "+finalPrice+"\n"+"Thank You.");
+   });
+
+   $("#delivery_yes").click(function(){
+    var deliveryAddress = prompt("Note: Delivery will be KSH:500 and 200+ per km.!\n Please fill your delivery address.");
+    var addingDelivery = finalPrice+500;
+    deliveryFinalPrice=addingDelivery;
+    console.log(deliveryFinalPrice);
+    console.log(deliveryAddress);
+    alert("Your order will be deliverd at: "+deliveryAddress+"\n"+"Please call: 0717976042 for confirmation.\n Your pizza order was.\n"+sizeSlecting+"@: "+sizePricing+"\n"+crustSelecting+"@: "+crustPricing+"\n"+toppingSelecting+"@: "+toppingPricing+"\n"+"Number of pizza's: "+pieces+"\nDelivery charge@: 500\n"+"Your total cost is. KCS: "+deliveryFinalPrice+"\n Please keep a screenshot of this receipt for confirmation on delivery. Thank YOu.");
+   });
+});
